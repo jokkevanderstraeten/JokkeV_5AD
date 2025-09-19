9e5750f2e0ce5ad82df0e6bc42b9981820be9c88
jokke <jokke@LQPTOP>
2025-09-19 09:47:59 +0200

    [Project_rekensomKlaar]

--- Project_rekensom/Project_rekensom/Program.cs
+++ Project_rekensom/Project_rekensom/Program.cs
@@ -4,36 +4,48 @@
 //Project Rekensom
 
 //velden
-byte _getal1 = 0;
-byte _getal2 = 0;
-byte _som = 0;
-byte _vermeerdering = 0;
-int _vermenigvuldiging = 0;
-byte _delen = 0;
-byte _uitkomst = 0;
+int _getal1 = 0;
+int _getal2 = 0;
+int _som = 0;
+int _vermeerderd = 0;
+int _vermenigvuldig = 0;
+double _delen = 0;
+
+const int _vermeerdering = 5;
+const int _deling = 2;
+const int _vermenigvuldiging = 10;
 //programma
 
 
 //Vraag getal 1 en sla het op
 Console.WriteLine("Geef jou eerste getal");
-_getal1 = byte.Parse(Console.ReadLine());
+_getal1 = int.Parse(Console.ReadLine());
 
 //Vraag getal 2 en sla het op
 Console.WriteLine("Geef jou tweede getal");
-_getal2 = byte.Parse(Console.ReadLine());
+_getal2 = int.Parse(Console.ReadLine());
 
 //bereken de som en sla deze op
-_som = Convert.ToByte(_getal1 + _getal2);
+_som = (_getal1 + _getal2);
 
 //vermeerder de som met 5 en sla op
-_vermeerdering = Convert.ToByte(_som + 5);
+_vermeerderd = (_som + _vermeerdering);
 
 //vermenigvuldiging de som met 10 en sla op
-_vermenigvuldiging = Convert.ToByte(_vermeerdering * 10);
+_vermenigvuldig = (_vermeerderd * _vermenigvuldiging);
 
 //Deel het getal door 2 en sla op
-_delen = Convert.ToByte(_vermenigvuldiging / 2);
-
-//
-_uitkomst = _delen;
+_delen = (_vermenigvuldig / _deling);
+//scherm leegmaken
+Console.Clear();
 // vul de tekst in en toon deze aan de gebruiker
+Console.WriteLine($"Uw gaf getallen {_getal1} en {_getal2} in." +
+    $"\r\nDe som hiervan is {_som}\r\nDit getal werd vermeerderd met 5. Dit gaf als uitkomst {_vermeerderd}" +
+    $"\r\nDaarna werd er vermenigvuldig met 10. Dit gaf als uitkomst {_vermenigvuldig}" +
+    $"\r\nAls laatste werd er gedeeld door 2. " +
+    $"\r\nHet uiteindelijke resultaat is {_delen}\r\n");
+
+
+Console.WriteLine("Druk op enter om af te sluiten");
+Console.ReadKey();
+return;
