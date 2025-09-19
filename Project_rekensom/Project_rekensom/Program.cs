43505c55207e6f6376dc46307ae69e0d46e38d69
jokkevanderstraeten <jokke.vanderstraeten@student.romerocollege.be>
2025-09-19 10:27:21 +0200

    Update Program.cs

--- Project_rekensom/Project_rekensom/Program.cs
+++ Project_rekensom/Project_rekensom/Program.cs
@@ -1,9 +1,9 @@
 ﻿
-//Jokke Van Der straeten
-//19/09/2025
-//Project Rekensom
+// Jokke Van Der straeten
+// 19/09/2025
+// Project Rekensom
 
-//velden
+// velden
 int _getal1 = 0;
 int _getal2 = 0;
 int _som = 0;
@@ -14,29 +14,29 @@ double _gedeeld = 0;
 const int _vermeerdering = 5;
 const int _deling = 2;
 const int _vermenigvuldiging = 10;
-//programma
+// programma
 
 
-//Vraag getal 1 en sla het op
+// Vraag getal 1 en sla het op
 Console.WriteLine("Geef jou eerste getal");
 _getal1 = int.Parse(Console.ReadLine());
 
-//Vraag getal 2 en sla het op
+// Vraag getal 2 en sla het op
 Console.WriteLine("Geef jou tweede getal");
 _getal2 = int.Parse(Console.ReadLine());
 
-//bereken de som en sla deze op
+// bereken de som en sla deze op
 _som = (_getal1 + _getal2);
 
-//vermeerder de som met 5 en sla op
+// vermeerder de som met 5 en sla op
 _vermeerderd = (_som + _vermeerdering);
 
-//vermenigvuldiging de som met 10 en sla op
+// vermenigvuldiging de som met 10 en sla op
 _vermenigvuldigd = (_vermeerderd * _vermenigvuldiging);
 
-//Deel het getal door 2 en sla op
+// Deel het getal door 2 en sla op
 _gedeeld = (_vermenigvuldigd / _deling);
-//scherm leegmaken
+// scherm leegmaken
 Console.Clear();
 // vul de tekst in en toon deze aan de gebruiker
 Console.WriteLine($"Uw gaf getallen {_getal1} en {_getal2} in." +
@@ -45,7 +45,7 @@ Console.WriteLine($"Uw gaf getallen {_getal1} en {_getal2} in." +
     $"\r\nAls laatste werd er gedeeld door 2. " +
     $"\r\nHet uiteindelijke resultaat is {_gedeeld}\r\n");
 
-
+//afsluiten van programma
 Console.WriteLine("Druk op enter om af te sluiten");
 Console.ReadKey();
 return;
