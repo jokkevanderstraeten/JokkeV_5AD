39c95806d3a412023b28f20d7bc5f7c13766f8af
jokke <jokke@LQPTOP>
2025-09-19 09:51:57 +0200

    [Correct]

--- Project_rekensom/Project_rekensom/Program.cs
+++ Project_rekensom/Project_rekensom/Program.cs
@@ -8,8 +8,8 @@ int _getal1 = 0;
 int _getal2 = 0;
 int _som = 0;
 int _vermeerderd = 0;
-int _vermenigvuldig = 0;
-double _delen = 0;
+int _vermenigvuldigd = 0;
+double _gedeeld = 0;
 
 const int _vermeerdering = 5;
 const int _deling = 2;
@@ -32,18 +32,18 @@ _som = (_getal1 + _getal2);
 _vermeerderd = (_som + _vermeerdering);
 
 //vermenigvuldiging de som met 10 en sla op
-_vermenigvuldig = (_vermeerderd * _vermenigvuldiging);
+_vermenigvuldigd = (_vermeerderd * _vermenigvuldiging);
 
 //Deel het getal door 2 en sla op
-_delen = (_vermenigvuldig / _deling);
+_gedeeld = (_vermenigvuldigd / _deling);
 //scherm leegmaken
 Console.Clear();
 // vul de tekst in en toon deze aan de gebruiker
 Console.WriteLine($"Uw gaf getallen {_getal1} en {_getal2} in." +
     $"\r\nDe som hiervan is {_som}\r\nDit getal werd vermeerderd met 5. Dit gaf als uitkomst {_vermeerderd}" +
-    $"\r\nDaarna werd er vermenigvuldig met 10. Dit gaf als uitkomst {_vermenigvuldig}" +
+    $"\r\nDaarna werd er vermenigvuldig met 10. Dit gaf als uitkomst {_vermenigvuldigd}" +
     $"\r\nAls laatste werd er gedeeld door 2. " +
-    $"\r\nHet uiteindelijke resultaat is {_delen}\r\n");
+    $"\r\nHet uiteindelijke resultaat is {_gedeeld}\r\n");
 
 
 Console.WriteLine("Druk op enter om af te sluiten");
