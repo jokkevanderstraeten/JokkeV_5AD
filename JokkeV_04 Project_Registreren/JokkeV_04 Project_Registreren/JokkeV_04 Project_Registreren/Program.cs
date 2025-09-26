cdaa5cb485369d66acb332ac59b842a01175ce1b
jokke <jokke@LQPTOP>
2025-09-26 10:54:10 +0200

    ]`les

---
+++ JokkeV_04 Project_Registreren/JokkeV_04 Project_Registreren/JokkeV_04 Project_Registreren/Program.cs
@@ -0,0 +1,14 @@
+﻿//jokke van der straeten
+//26/09/25
+// oefening 04 project_Registreren
+
+
+//velden
+
+string _wachtwoordinstellen;
+string _loginnaaminstellen;
+//programma
+//Stap 1: Intro
+Console.WriteLine("Hallo, registreer je op onze website");
+Console.WriteLine("Druk op een toets om verder te gaan");
+Console.ReadKey();
