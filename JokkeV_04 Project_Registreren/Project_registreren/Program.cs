5199b1a5c5209cca457cf5c3f32550840f60cf9d
jokke <jokke@LQPTOP>
2025-09-26 11:04:07 +0200

    [les]

--- JokkeV_04 Project_Registreren/Project_registreren/Program.cs
+++ JokkeV_04 Project_Registreren/Project_registreren/Program.cs
@@ -12,3 +12,10 @@ string _loginnaaminstellen;
 Console.WriteLine("Hallo, registreer je op onze website");
 Console.WriteLine("Druk op een toets om verder te gaan");
 Console.ReadKey();
+Console.Clear();
+//naam vragen + opslaan
+Console.WriteLine("Wat wil je gebruiken als Loginnaam?");
+_loginnaaminstellen = (Console.ReadLine());
+//wachtwoord vragen + opslaan
+Console.WriteLine("Wat wil je gebruiken als wachtwoord?");
+_wachtwoordinstellen = (Console.ReadLine());
