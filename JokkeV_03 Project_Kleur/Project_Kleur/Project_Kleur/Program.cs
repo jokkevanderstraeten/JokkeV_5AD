53a8b38226bfcb0a63ba01fed03793b18510bb97
jokke <jokke@LQPTOP>
2025-09-26 09:32:10 +0200

    [les]

---
+++ JokkeV_03 Project_Kleur/Project_Kleur/Project_Kleur/Program.cs
@@ -0,0 +1,37 @@
+﻿//Jokke Van Der straeten
+//26/09/25
+//Oef 16 Project_Kleur
+
+//Velden
+
+byte _Keuze;
+
+//Programma
+
+
+//Stap 1: toon de keuzes (rood, groen of blauw)
+Console.WriteLine("1. Rood\n2. Groen\n3. Blauw ");
+Console.WriteLine("kies een kleur");
+try
+{ 
+    //stap 2: vraag de gebruiker werlke kleur + sla op
+    _Keuze = Byte.Parse(Console.ReadLine());
+
+    //stap 3:Maak het scherm leeg    
+    Console.Clear();
+    //stap 4: als de keuze rood is toon rood
+    if (_Keuze == 1)
+        Console.WriteLine($"je koos de volgende kleur: Rood");
+    //stap 4: als de keuze groen is toon  groen
+    else if (_Keuze == 2)
+        Console.WriteLine($"je koos de volgende kleur: Groen");
+
+    //stap 4: als de keuze blauw is toon blauw    
+    else
+        Console.WriteLine($"je koos de volgende kleur: Blauw");
+    return;
+}
+catch
+{
+    Console.WriteLine("geef 1,2 of 3 in");
+}
