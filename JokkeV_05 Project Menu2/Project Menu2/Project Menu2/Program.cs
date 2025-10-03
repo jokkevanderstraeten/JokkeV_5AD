648927e3f0acd53864c2b0c4bfed0b4e2d9db5b9
jokke <jokke@LQPTOP>
2025-10-03 09:54:25 +0200

    [les]

--- JokkeV_05 Project Menu2/Project Menu2/Project Menu2/Program.cs
+++ JokkeV_05 Project Menu2/Project Menu2/Project Menu2/Program.cs
@@ -3,12 +3,11 @@
 //Project06 Menu2
 
 //Velden
-using System.Net.Sockets;
 
+Boolean _herhalen = true;
 Byte _keuze;
 int _getal1 = 0;
 int _getal2 = 0;
-Boolean _herhalen = true;
 int _UITKOMSTVER = 0;
 int _UITKOMSTMIN = 0;
 int _UITKOMSTPLUS = 0;
@@ -16,6 +15,8 @@ int _UITKOMSTPLUS = 0;
 
 // Intro
 Console.WriteLine("Welkom bij ons rekenprogramma");
+Console.ReadKey
+//scherm leegmaken
 Console.Clear();
 do
 {
@@ -24,18 +25,22 @@ do
         //vraag getal1 + opslaan
         Console.WriteLine("Welk getal wil je gebruiken als eerste getal?");
         _getal1 = int.Parse(Console.ReadLine());
+        //scherm leegmaken
         Console.Clear();
 
         //vraag getal2 + opslaan
         Console.WriteLine("Welk getal wil je gebruiken als tweede getal?");
         _getal2 = int.Parse(Console.ReadLine());
+        //scherm leegmaken
         Console.Clear();
 
         //Vraag of hij wilt  Vermenigvuldigen, optellen of verminderen of asluiten + sla op
         Console.WriteLine("Kies uit deze keuzes");
         Console.WriteLine("1. Vermenigvuldigen\n2. optellen\n3. verminderen\n4. afsluiten");
         _keuze = Byte.Parse(Console.ReadLine());
+        //scherm leegmaken
         Console.Clear();
+        
         if (_keuze == 1)
         {
             // als hij kiest voor vermenigvuldigen doe dan getal1 * getal2
@@ -62,12 +67,14 @@ do
         }
         else if (_keuze == 4)
         {
+            //afsluiten
             Console.WriteLine("daag");
             _herhalen = false;
             return;
         }
         else
         {
+            //foutmelding
             Console.WriteLine("Er ging iets mis, geef een juist getal in");
         }
     }
