66892a314e32af66105374a0b45c2c0b61ae1db3
jokke <jokke@LQPTOP>
2025-10-06 08:42:22 +0200

    [les]

---
+++ JokkeV_07 Project Illy/Project_07Illy/Project_07Illy/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] _best = new int[5];
+int _keuzeInt = 0;
+for (int i = 0; i < _best.Length; i++)
+{
+    Console.WriteLine($"Geef getal {i} in:");
+    _best[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Welk getal wil je zien?");
+_keuzeInt = int.Parse(Console.ReadLine()); 
+for (int i = 0; i < _keuzeInt; i++)
+{
+    Console.Write(_best[i =_keuzeInt] + " ");
+}
