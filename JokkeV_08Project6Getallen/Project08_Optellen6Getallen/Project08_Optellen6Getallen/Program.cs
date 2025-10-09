868a87492b512c6fac2603dcdd1521e676ea5749
jokke <jokke@LQPTOP>
2025-10-09 11:51:14 +0200

    [Yanto]

--- JokkeV_08Project6Getallen/Project08_Optellen6Getallen/Project08_Optellen6Getallen/Program.cs
+++ JokkeV_08Project6Getallen/Project08_Optellen6Getallen/Project08_Optellen6Getallen/Program.cs
@@ -5,11 +5,11 @@
 
 
 //velden
- int[] _getallen = new int[6];
+int[] _getallen = new int[6];
 int i = 0;
-
 Boolean _herhalen = true;
 //programma
+//intro
 Console.WriteLine("Welkom bij mijn programma:");
 Console.WriteLine("Druk op een toets om verder te gaan");
 Console.ReadKey();
@@ -17,21 +17,27 @@ do
 {
     try
     {
+        //vraag 6 getallen en sla deze op
         Console.WriteLine("Geef 6 getallen in");
         for (i = 0; i < _getallen.Length; i++)
         {
+            //toon welk getal hij ingeeft
             Console.WriteLine($"Geef getal {i}");
             _getallen[i] = int.Parse(Console.ReadLine());
         }
+        //scherm leegmaken
         Console.Clear();
+      // Bereken de som 
         int _som = _getallen[0] + _getallen[1] + _getallen[2] + _getallen[3] + _getallen[4] + _getallen[5];
+        // toon de som en wacht om af te sluiten
+        Console.WriteLine($"De som is {_som}");
         Console.ReadKey();
         _herhalen = false;
+
     }
 
-
+    //foutmelding
     catch { Console.WriteLine("geef een juist getal in"); }
             
 }       
-
 while (_herhalen);
