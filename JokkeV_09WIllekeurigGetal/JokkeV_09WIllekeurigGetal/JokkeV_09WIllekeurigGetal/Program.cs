dc7bef14e8b7293fa0c4a3670e895e310e134070
jokke <jokke@LQPTOP>
2025-10-10 10:47:22 +0200

    [Les ]

--- JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/Program.cs
+++ JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/Program.cs
@@ -1,7 +1,25 @@
-﻿//Jokke Van Der Straeten
-//10/10/2025
-//Project09WillekeurigGetal
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//velden
+namespace JokkeV_09WIllekeurigGetal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jokke Van Der Straeten
+            //10/10/2025
+            //Project09WillekeurigGetal
 
-//Programma
+            //velden
+
+            //Programma
+
+
+
+        }
+    }
+}
