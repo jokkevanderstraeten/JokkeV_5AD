97dceb438f12396294763e96d9f944abbf8938dd
jokke <jokke@LQPTOP>
2025-10-14 19:34:49 +0200

    [thuis]

--- JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/Program.cs
+++ JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/JokkeV_09WIllekeurigGetal/Program.cs
@@ -12,12 +12,19 @@ namespace JokkeV_09WIllekeurigGetal
         {
             //Jokke Van Der Straeten
             //10/10/2025
-            //Project09WillekeurigGetal
+            //Project09_WillekeurigGetal
 
             //velden
+            Random _rdm = new Random();
+            
 
+            int _Getal1 = 0;
+            int _Getal2 = 0;
             //Programma
+            _Getal1 = _rdm.Next(11);
+            _Getal2 = _rdm.Next(11);
 
+            Console.WriteLine($"{_Getal1} is je eerste getal,{_Getal2} is je tweede getal");
 
 
         }
---
+++ JokkeV_10Dobbel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokkeV_10Dobbel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jokke Van Der Straeten
+            //14/10/2025
+            //Project10 Dobbel
+            
+            //velden
+            Byte _Keuze = 0;
+            Byte _getal = 0;
+            Random _rdm = new Random();
+            Boolean _herhalen = true;
+            //intro
+            Console.WriteLine("Hallo, welkom bij mijn dobbelsteen\ndruk om verder te gaan");
+            Console.ReadKey();
+            //Scherm leegmaken
+            Console.Clear();
+            //keuze vragen en opslaan
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Wat wil je kiezen?");
+                    Console.WriteLine("1. Dobbelsteen Gooien \n2. Afsluiten");
+                    _Keuze = Byte.Parse(Console.ReadLine());
+                    //scherm leegmaken
+                    Console.Clear();
+                    //als keuze = 1 geef een getal van 1-6
+                    if (_Keuze == 1)
+                    {
+                        _getal = Convert.ToByte(_rdm.Next(0, 7));
+                        Console.WriteLine($"De dobbelsteen stopt op {_getal}");
+                        _herhalen = false;
+                    }
+                    //als keuze = 2 sluit af en toon bericht
+                    else if (_Keuze == 2)
+                    {
+                        Console.WriteLine("Afgesloten");
+                        return;
+                        _herhalen = false;
+                    }
+                    //als er iets anders is geef foutmelding
+                    else {Console.WriteLine("Er ging iets mis kies 1 of 2"); }
+                }
+
+                catch {Console.WriteLine("Er ging iets mis kies 1 of 2"); }
+            }
+            while (_herhalen);
+            
+           
+        }
+    }
+}
