75eb13c3cb55d2311dc0964d9c88847bea2cbeaa
jokke <jokke@LQPTOP>
2025-10-16 14:25:22 +0200

    [les]

--- JokkeV_11VrijeDobbel/Project11_VrijeDobbel/Program.cs
+++ JokkeV_11VrijeDobbel/Project11_VrijeDobbel/Program.cs
@@ -24,7 +24,7 @@ namespace Project11_VrijeDobbel
             //programma
             try
             {
-                //intro
+                //stap 1: intro
                 Console.WriteLine("Welkom bij het programma.");
                 Console.WriteLine("Tik op een toets om verder te gaan...");
                 Console.ReadKey();
@@ -32,49 +32,51 @@ namespace Project11_VrijeDobbel
 
                 do
                 {
-                    //Vragen wat hij wilt doen + Opslaan
-                    Console.WriteLine("Wat wil je doen?");
-                    Console.WriteLine("1. Gooien met de dobbelsteen \n2. Afsluiten");
-                    _keuze = Byte.Parse(Console.ReadLine());
-
-                    //als gebruiker kiest voor 1 vraag een max en min waarden + geef getal
-                    if (_keuze == 1)
+                    try
                     {
-                        //min waarde
-                        Console.WriteLine("Geef een Minimum waarde in");
-                        _minWaarde = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        //max waarde
-                        Console.WriteLine("Geef een Maximum waarde in");
-                        _maxWaarde = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        //getal geven
-                        _getal = random.Next(_minWaarde, _maxWaarde + 1);
-                        Console.WriteLine($"De dobbelsteen geeft {_getal}");
-                        _herhalen = false;
+                        //Keuzemenu tonen en keuze  Opslaan
+                        Console.WriteLine("Wat wil je doen?");
+                        Console.WriteLine("1. Gooien met de dobbelsteen \n2. Afsluiten");
+                        _keuze = Byte.Parse(Console.ReadLine());
 
-                    }
-                    //als gebruiker kiest voor 2 sluit dan het programma af
-                    else if (_keuze == 2)
-                    {
-                        Console.WriteLine("Oke, Dag");
-                        _herhalen = false;
-                        return;
-                    }
-                    //foutmelding bij if statements
-                    else
-                    { Console.WriteLine("Er ging iets mis, geef een juiste keuze in"); }
-                    if (_getal == 67 )
-                    { Console.WriteLine("YANTOOOO 67777777");
-                        _herhalen = false;
-                    }                
-                
+                        //als gebruiker kiest voor 1 vraag een max en min waarden + geef getal
+                        if (_keuze == 1)
+                        {
+                            //min waarde vragen + opslaan
+                            Console.WriteLine("Geef een Minimum waarde in");
+                            _minWaarde = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            //max waarde + opslaan
+                            Console.WriteLine("Geef een Maximum waarde in");
+                            _maxWaarde = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            //getal bepalen en tonen
+                            _getal = random.Next(_minWaarde, _maxWaarde + 1);
+                            Console.WriteLine($"De dobbelsteen geeft {_getal}");
+                            _herhalen = false;
 
-                }
-                while (_herhalen);
+                        }
+                        //als gebruiker kiest voor 2 sluit het programma af en toon de tekst
+                        else if (_keuze == 2)
+                        {
+                            Console.WriteLine("Oke, Dag");
+                            _herhalen = false;
+                            return;
+                        }
+                        //Foutmelding
+                        else
+                        { Console.WriteLine("Er ging iets mis, geef een juiste keuze in"); }
+                    }
+                    catch
+                    {//Scherm leegmaken
+                        Console.Clear();
+                        //foutmelding
+                        Console.WriteLine("Je gaf een verkeerd keuze in.");
+                    }
+               } while (_herhalen);
+
+
             }
-            //foutmelding
-            catch { Console.WriteLine("Je gaf een verkeerd keuze in."); }
-        }   
-    }   
+        }
+    }
 }
